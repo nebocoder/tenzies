9d4f3f4de216323dbda8d8a9a284dbf0a2145554
Nemanja <91620216+nebocoder@users.noreply.github.com>
2022-03-24 19:14:30 +0100

    Add held dice background color

--- src/Die.js
+++ src/Die.js
@@ -1,6 +1,10 @@
 export default function Die(props) {
+  const styles = {
+    backgroundColor: props.isHeld ? '#59e391' : '#fff',
+  };
+
   return (
-    <div className="die-face">
+    <div className="die-face" style={styles}>
       <h2 className="die-num">{props.value}</h2>
     </div>
   );
