2109b33a9d3e9fc1d5dd46985e201a1daf1b75c5
Nemanja <91620216+nebocoder@users.noreply.github.com>
2022-03-24 18:50:10 +0100

    Turn dice state into object and add nanoid

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import './style.css';
 import Die from './Die';
+import { nanoid } from 'nanoid';
 
 export default function App() {
   const [dice, setDice] = React.useState(allNewDice());
 
   function allNewDice() {
-    return new Array(10).fill().map(() => Math.floor(Math.random() * 6) + 1);
+    return new Array(10).fill().map(() => ({
+      id: nanoid(),
+      value: Math.floor(Math.random() * 6) + 1,
+      isHeld: false,
+    }));
   }
 
   function rollDice() {
     setDice(allNewDice());
   }
 
-  const diceElements = dice.map((num) => <Die value={num} />);
+  const diceElements = dice.map((die) => (
+    <Die key={die.id} value={die.value} />
+  ));
 
   return (
     <main>
