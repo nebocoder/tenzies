1aca79f811eeb9292d7de0653f626c841af9d617
Nemanja <91620216+nebocoder@users.noreply.github.com>
2022-03-24 17:48:03 +0100

    Dice styling and media query for smaller screens

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
 import './style.css';
+import Die from './Die';
 
 export default function App() {
-  return <main></main>;
+  return (
+    <main>
+      <div className="dice-container">
+        <Die value={1} />
+        <Die value={5} />
+        <Die value={5} />
+        <Die value={5} />
+        <Die value={3} />
+        <Die value={5} />
+        <Die value={4} />
+        <Die value={5} />
+        <Die value={5} />
+        <Die value={5} />
+      </div>
+    </main>
+  );
 }
